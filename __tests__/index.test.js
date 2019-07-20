2b762eed7355a3da73d215116f7999d9f88d923e
d.sukhinin <d.sukhinin@tinkoff.ru>
2019-07-20 13:51:09 +0300

    refactored tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,11 +8,12 @@ describe.each(formats)('genDiff | сравнение двух файлов', (fo
   const pathToFixtures = '__tests__/__fixtures__/files';
   const pathToFile1 = path.join(pathToFixtures, `before.${format}`);
   const pathToFile2 = path.join(pathToFixtures, `after.${format}`);
+  const fixturesPath = `${__dirname}/__fixtures__`;
 
-  describe('Если передан не передан формат (формат tree)', () => {
+  describe('Если не передан формат (формат tree)', () => {
     test(`То возвращается результат сравнения ${format}-файлов в формате дерева`, () => {
       const actualResult = genDiff(pathToFile1, pathToFile2);
-      const expectedResult = fs.readFileSync(`${__dirname}/__fixtures__/diff-result-tree.txt`, 'utf8');
+      const expectedResult = fs.readFileSync(`${fixturesPath}/diff-result-tree.txt`, 'utf8');
 
       expect(actualResult).toEqual(expectedResult);
     });
@@ -21,7 +22,7 @@ describe.each(formats)('genDiff | сравнение двух файлов', (fo
   describe('Если передан формат plain', () => {
     test(`То возвращается результат сравнения ${format}-файлов в плоском формате`, () => {
       const actualResult = genDiff(pathToFile1, pathToFile2, 'plain');
-      const expectedResult = fs.readFileSync(`${__dirname}/__fixtures__/diff-result-plain.txt`, 'utf8');
+      const expectedResult = fs.readFileSync(`${fixturesPath}/diff-result-plain.txt`, 'utf8');
 
       expect(actualResult).toEqual(expectedResult);
     });
@@ -30,7 +31,7 @@ describe.each(formats)('genDiff | сравнение двух файлов', (fo
   describe('Если передан формат json', () => {
     test(`То возвращается результат сравнения ${format}-файлов в формате json`, () => {
       const actualResult = genDiff(pathToFile1, pathToFile2, 'json');
-      const expectedResult = fs.readFileSync(`${__dirname}/__fixtures__/diff-result.json`, 'utf8');
+      const expectedResult = fs.readFileSync(`${fixturesPath}/diff-result.json`, 'utf8');
 
       expect(actualResult).toEqual(expectedResult);
     });
