94b8fff1135cfd49ee8609be587a1b70e2a4bc45
d.sukhinin <d.sukhinin@tinkoff.ru>
2019-07-22 09:49:18 +0300

    refactored after review

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,36 +4,30 @@ import genDiff from '../src';
 
 const formats = ['json', 'yml', 'ini'];
 
-describe.each(formats)('genDiff | сравнение двух файлов', (format) => {
+describe.each(formats)('genDiff | generating difference between files', (format) => {
   const pathToFixtures = '__tests__/__fixtures__/files';
   const pathToFile1 = path.join(pathToFixtures, `before.${format}`);
   const pathToFile2 = path.join(pathToFixtures, `after.${format}`);
   const fixturesPath = `${__dirname}/__fixtures__`;
 
-  describe('Если не передан формат (формат tree)', () => {
-    test(`То возвращается результат сравнения ${format}-файлов в формате дерева`, () => {
-      const actualResult = genDiff(pathToFile1, pathToFile2);
-      const expectedResult = fs.readFileSync(`${fixturesPath}/diff-result-tree.txt`, 'utf8');
+  test(`should return the result of comparing ${format}-files in tree format`, () => {
+    const actualResult = genDiff(pathToFile1, pathToFile2);
+    const expectedResult = fs.readFileSync(`${fixturesPath}/diff-result-tree.txt`, 'utf8');
 
-      expect(actualResult).toEqual(expectedResult);
-    });
+    expect(actualResult).toEqual(expectedResult);
   });
 
-  describe('Если передан формат plain', () => {
-    test(`То возвращается результат сравнения ${format}-файлов в плоском формате`, () => {
-      const actualResult = genDiff(pathToFile1, pathToFile2, 'plain');
-      const expectedResult = fs.readFileSync(`${fixturesPath}/diff-result-plain.txt`, 'utf8');
+  test(`should return the result of comparing ${format}-files in plain format`, () => {
+    const actualResult = genDiff(pathToFile1, pathToFile2, 'plain');
+    const expectedResult = fs.readFileSync(`${fixturesPath}/diff-result-plain.txt`, 'utf8');
 
-      expect(actualResult).toEqual(expectedResult);
-    });
+    expect(actualResult).toEqual(expectedResult);
   });
 
-  describe('Если передан формат json', () => {
-    test(`То возвращается результат сравнения ${format}-файлов в формате json`, () => {
-      const actualResult = genDiff(pathToFile1, pathToFile2, 'json');
-      const expectedResult = fs.readFileSync(`${fixturesPath}/diff-result.json`, 'utf8');
+  test(`should return the result of comparing ${format}-files in json format`, () => {
+    const actualResult = genDiff(pathToFile1, pathToFile2, 'json');
+    const expectedResult = fs.readFileSync(`${fixturesPath}/diff-result.json`, 'utf8');
 
-      expect(actualResult).toEqual(expectedResult);
-    });
+    expect(actualResult).toEqual(expectedResult);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import _ from 'lodash';
 import parsers from './parsers';
 import parseToAST from './parseToAst';
 import { formatToTree, formatToPlain, formatToJSON } from './formatters';
